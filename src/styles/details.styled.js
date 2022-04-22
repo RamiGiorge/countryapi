ca4f989c358028435355f1277b63f203a5c0996f
Rome <rami.giorge@gmail.com>
2022-04-23 01:44:20 +0300

    Fix Details Page Responsiveness

--- src/styles/details.styled.js
+++ src/styles/details.styled.js
@@ -1,56 +1,73 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const StyledDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+  padding: 40px;
+  background: var(--background);
+  min-height: 100%;
+  transition: all 0.2s ease;
+
+  button {
+    align-self: flex-start;
+    padding: 20px;
+    font-weight: 700;
+    color: var(--dark-gray);
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+    box-shadow: -8px -8px 15px #fff, 8px 8px 15px #babecc;
+    transition: all 0.2s ease;
+    &:hover {
+      transform: scale(1.1);
+      color: var(--text);
+      span {
+        color: var(--text);
+        font-weight: 700;
+      }
+    }
+
+    span {
+      transition: all 0.2s ease;
+    }
+  }
+
+  div {
+    width: 30%;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    gap: 40px;
-    padding: 40px;
-    background: var(--background);
-    min-height: 100%;
-    transition: all 0.2s ease;
+    justify-content: center;
+    gap: 20px;
+    box-shadow: -8px -8px 15px #fff, 8px 8px 15px #babecc;
+    padding: 20px;
+    border-radius: 20px;
+    font-weight: 700;
+    transition: width .4s ease;
 
-    button {
-        align-self: flex-start;
-        padding: 20px;
-        font-weight: 700;
-        color: var(--dark-gray);
-        border-radius: 10px;      
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        gap: 10px;
-        box-shadow: -8px -8px 15px #fff, 8px 8px 15px #babecc;
-        transition: all 0.2s ease;
-        &:hover {
-            transform: scale(1.1);
-            color: var(--text);
-            span {
-                color: var(--text);
-                font-weight: 700;
-            }
-        }
-
-        span {
-            transition: all 0.2s ease;
-        }
+    h1 {
+      text-align: center;
+    }
+    
+    @media (max-width: ${({ theme }) => theme.breakpoints.xl}) {
+      width: 35%;
     }
 
-    div {
-        width: 30%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        gap: 20px;
-        box-shadow: -8px -8px 15px #fff, 8px 8px 15px #babecc;
-        padding: 20px;
-        border-radius: 20px;
-        font-weight: 700;
-
-        h1 {
-            text-align: center;
-        }
+    @media (max-width: ${({ theme }) => theme.breakpoints.large}) {
+      width: 50%;
     }
-`
 
-export default StyledDetails
+    @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
+      width: 70%;
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.small}) {
+      width: 100%;
+    }
+  }
+`;
+
+export default StyledDetails;
--- src/styles/theme.styled.js
+++ src/styles/theme.styled.js
@@ -4,6 +4,8 @@ const theme = {
         warning: '#f13a68',
     },
     breakpoints: {
+        xl: '1200px',
+        large: '992px',
         medium: '690px',
         small: '550px'
     }
